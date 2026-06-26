9130e9963be1bcb71c035df237247fccd0ac23c8
anuj-tiwari-iphtech <anuj.tiwari@iphtechnologies.com>
2026-06-26 15:11:23 +0530

    Changes made by nayan

--- git.js
+++ git.js
@@ -1,3 +1,11 @@
 console.log("git command")
 
 console.log(" The Thing is a best super Hero")
+
+console.log("I am using Anuj's PC")
+
+function fruits(a , b){
+    return ("Sum is ", a+b)
+}
+
+console.log(fruits(41,45))
